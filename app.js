a8363caa867f215c2707e1e85dfc6850ad62b6d1
KarlOlsson90 <karl.olsson.mail@gmail.com>
2020-09-18 15:34:52 +0200

    pair programmed route index as final foundation

--- app.js
+++ app.js
@@ -3,4 +3,10 @@ const app = express()
 
 app.use( express.static('public') )
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const routes = require('./routes/')
+app.use('/', routes)
+
 module.exports = app
