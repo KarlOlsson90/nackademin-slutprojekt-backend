48a7bad3fdabec8054dfb76adb6a4537c97727a2
Tabben <nitharsan.nagularajan@yh.nackademin.se>
2020-09-21 10:56:16 +0200

    added ordersModel and unit tests for it

---
+++ models/ordersModel.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose')
+
+const order = new mongoose.Schema({
+    customerId: String,
+    status: String,
+    items: Array,
+    value: Number
+})
+
+order.set('timestamps', true)
+
+const ordersModel = mongoose.model('orders', order)
+
+module.exports = {
+    addOrder:  (customerId, items = [], value) => {
+        const status = 'inProcess'
+
+        const order = {
+            customerId: customerId,
+            status: status,
+            items: items,
+            value: value
+        }
+        return new Promise(async(resolve, reject) => {
+            try {
+                const insert = await ordersModel.create(order)
+                resolve(insert)
+            } catch (error) {
+                reject(error)
+            }
+        }) 
+    },
+    findOrder:  (orderId) => {
+        
+        return new Promise(async(resolve, reject) => {
+            try {
+                const order = await ordersModel.findOne({_id: orderId})
+                resolve(order)
+            } catch (error) {
+                reject(error)
+            }
+        }) 
+    },
+    updateOrder: (orderId, order) => {
+
+        return new Promise(async(resolve, reject) => {
+            try {
+                const update = await ordersModel.updateOne(
+                    {_id: orderId}, 
+                    {
+                        $set: {
+                            items: order.items,
+                            value: order.value
+                        }
+                    }
+                )
+                resolve('Order was Updated')
+            } catch (error) {
+                reject(error)
+            }
+        })
+    },
+    deleteOrder: (orderId) => {
+        return new Promise(async(resolve, reject) => {
+            try {
+                await ordersModel.deleteOne({_id: orderId})
+                resolve('Deleted')
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+}
